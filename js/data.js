e3b882ed86e1da7825fce095a533cf66a9601535
Vladimir Traikov <traikov.vladimir@emabg.com>
2016-04-17 18:09:12 +0300

    Updated images
    Signed-off-by: Vladimir Traikov <traikov.vladimir@emabg.com>

--- js/data.js
+++ js/data.js
@@ -3,37 +3,37 @@
 categoriesShowAdditionalList = [1,2,3];
 
 categories = [
-    { name: "125A", image: "grid4.jpg" },
-    { name: "160A", image: "grid5.jpg" },
-    { name: "250A", image: "grid6.jpg" },
-    { name: "630A", image: "grid7.jpg" },
-    { name: "Accessories", image: "grid7.jpg" },
+    { name: "125A", image: "front.png" },
+    { name: "160A", image: "grid160.jpg" },
+    { name: "250A", image: "250-6.jpg" },
+    { name: "630A", image: "grid630.jpg" },
+    { name: "Accessories", image: "gridA.jpg" },
   ];
 
 products = [
   // category 125A
   [
     // product
-    { name: "2 rolls",
-      description: "430x520",
+    { name: "510 x 435 x 175",
+      description: "2 rolls",
       details: "Aluminium Electricl board up to 125A",
       thumbnail: "2reda.jpg",
       images: ["show.jpg", "show1.jpg", "show2.jpg", "show3.jpg" ],
       downloads: []
     },
     // product
-    { name: "3 ROLLS",
-      description: "430x520",
+    { name: "675 x 435 x 175",
+      description: "3 rolls",
       details: "",
       thumbnail: "3reda.jpg",
       images: ["show.jpg"],
       downloads: []
     },
     // product
-    { name: "4 ROLLS",
-      description: "430x520",
+    { name: "825 x 435 x 175",
+      description: "4 rolls",
       details: "",
-      thumbnail: "3reda.jpg",
+      thumbnail: "4reda.jpg",
       images: ["show.jpg"],
       downloads: []
     }
@@ -42,68 +42,138 @@ products = [
   // category 160A
   [
     // product
-    { name: "Suspendisse consectetur",
+    { name: "2 ROLLS",
  description: "",
       details: "",
-      thumbnail: "grid4.jpg",
+      thumbnail: "160-2.jpg",
       images: ["show.jpg"],
       downloads: []
     },
     // product
-    { name: "Pellentesque",
+    { name: "3 ROLLS",
       description: "",
       details: "",
-      thumbnail: "grid6.jpg",
+      thumbnail: "160-3.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "4 ROLLS",
+      description: "",
+      details: "",
+      thumbnail: "160-4.jpg",
       images: ["show.jpg"],
       downloads: []
     }
   ],
 
-  // category 630A
+  // category 250A
   [
     // product
-    { name: "Suspendisse consectetur",
+    { name: "5 РЕДА",
       description: "",
       details: "",
-      thumbnail: "grid3.jpg",
-      images: ["show1.jpg"],
-      downloads: []
-    },
-    // product
-    { name: "Pellentesque",
-      description: "",
-      details: "",
-      thumbnail: "grid6.jpg",
-      images: ["show2.jpg"],
-      downloads: []
-    },
-    // product
-    { name: "Pellentesque",
-      description: "",
-      details: "",
-      thumbnail: "grid5.jpg",
+      thumbnail: "250-5.jpg",
       images: ["show.jpg"],
       downloads: []
     },
     // product
-    { name: "Pellentesque",
+    { name: "6 РЕДА",
       description: "",
       details: "",
-      thumbnail: "grid7.jpg",
+      thumbnail: "250-6.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "9 РЕДА",
+      description: "",
+      details: "",
+      thumbnail: "250-9.jpg",
       images: ["show.jpg"],
       downloads: []
     }
   ],
 
+  // category 630А
+  [
+    // product
+    { name: "600х400х1805",
+      description: "",
+      details: "",
+      thumbnail: "600х400х1805.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    },
+    { name: "800х400х1805",
+      description: "",
+      details: "",
+      thumbnail: "800х400х1805.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    },
+    { name: "1000х400х1505",
+      description: "",
+      details: "",
+      thumbnail: "1000х400х1505.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    },
+    { name: "1000х400х1955",
+      description: "",
+      details: "",
+      thumbnail: "1000х400х1955.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    },
+    { name: "1200х350х1505",
+      description: "",
+      details: "",
+      thumbnail: "1200х350х1505.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    },
+    { name: "1200х400х1955",
+      description: "",
+      details: "",
+      thumbnail: "1200х400х1955.jpg",
+      images: ["show630.jpg"],
+      downloads: []
+    }
+  ],
   // category Accessories
   [
     // product
-    { name: "Test",
+    { name: "a1",
+ description: "",
+      details: "",
+      thumbnail: "a1.jpg",
+      images: ["a1.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "a2",
       description: "",
       details: "",
-      thumbnail: "grid4.jpg",
-      images: ["show1.jpg"],
+      thumbnail: "a2.jpg",
+      images: ["a2.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "a3",
+      description: "",
+      details: "",
+      thumbnail: "a3.jpg",
+      images: ["a3.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "a4",
+      description: "",
+      details: "",
+      thumbnail: "a4.jpg",
+      images: ["a4.jpg"],
       downloads: []
     }
-  ],
+  ]
 ]
